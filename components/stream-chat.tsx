e12a19b2cee7fcd4273e0d663e95cefe3a3521f2
orgamiryazdani <orgamiryazdani@gmail.com>
2025-04-19 19:52:37 +0330

    feat: Add user selector and sidbar and fix style

--- components/stream-chat.tsx
+++ components/stream-chat.tsx
@@ -1,25 +1,60 @@
 'use client'
 
-import { createToken } from '@/lib/actions'
+import Link from 'next/link'
 import { cn } from '@/lib/utils'
-import { useTheme } from 'next-themes'
 import { useCallback } from 'react'
-import { ChannelFilters, ChannelOptions, ChannelSort } from 'stream-chat'
+import { useTheme } from 'next-themes'
+import { createToken } from '@/lib/actions'
+
+import type {
+  ChannelSort,
+  ChannelFilters,
+  ChannelOptions,
+  Channel as ChannelType
+} from 'stream-chat'
+
 import {
-  useCreateChatClient,
   Chat,
   Channel,
   ChannelHeader,
+  ChannelList,
   MessageInput,
   MessageList,
   Thread,
   Window,
-  ChannelList,
+  useCreateChatClient,
   DefaultStreamChatGenerics
 } from 'stream-chat-react'
-import 'stream-chat-react/dist/css/v2/index.css'
+
 import { EmojiPicker } from 'stream-chat-react/emojis'
-import { SearchIndex } from 'emoji-mart';
+import { init, SearchIndex } from 'emoji-mart'
+import data from '@emoji-mart/data'
+init({ data })
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
+
+import { UserButton } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/theme-toggle'
+import CustomListContainer from '@/components/custom-list-container'
+
+import {
+  Dock,
+  Megaphone,
+  MessageCircleMore,
+  MessageSquare,
+  Scan,
+  UsersRound
+} from 'lucide-react'
+
+import { HomeIcon } from '@radix-ui/react-icons'
+
+import 'stream-chat-react/dist/css/v2/index.css'
 
 interface StreamChatProps {
   userData: {
@@ -31,6 +66,7 @@ interface StreamChatProps {
 
 export default function StreamChat({ userData }: StreamChatProps) {
   const { resolvedTheme } = useTheme()
+
   const tokenProvider = useCallback(async () => {
     return await createToken(userData.id)
   }, [userData.id, createToken])
@@ -50,7 +86,9 @@ export default function StreamChat({ userData }: StreamChatProps) {
     limit: 10
   }
 
-  if (!client) return <div>Setting up client & connection...</div>
+  if (!client) {
+    return null
+  }
 
   return (
     <Chat
@@ -61,13 +99,171 @@ export default function StreamChat({ userData }: StreamChatProps) {
           : 'str-chat__theme-light'
       )}
     >
-      <ChannelList sort={sort} filters={filters} options={options} />
+      <aside className='inset-y z-20 flex h-full flex-col border-r'>
+        <nav className='grid gap-1 px-2 py-4'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='Playground'
+                  asChild
+                >
+                  <Link href='/'>
+                    <HomeIcon className='size-5' />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Home
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg bg-muted'
+                  aria-label='Playground'
+                >
+                  <MessageSquare className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Chats
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='Models'
+                >
+                  <UsersRound className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Communities
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='API'
+                >
+                  <Scan className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Status
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='Documentation'
+                >
+                  <MessageCircleMore className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Channels
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='Settings'
+                >
+                  <Dock className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Tools
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='ghost'
+                  size='icon'
+                  className='rounded-lg'
+                  aria-label='Settings'
+                >
+                  <Megaphone className='size-5' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Advertise
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+
+        <nav className='mt-auto grid gap-2 px-2 py-4'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <ThemeToggle />
+              </TooltipTrigger>
+              <TooltipContent side='right' sideOffset={5}>
+                Theme
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <div className='flex items-center justify-center'>
+            <UserButton />
+          </div>
+        </nav>
+      </aside>
+
+      <ChannelList
+        sort={sort}
+        filters={filters}
+        options={options}
+        List={CustomListContainer}
+        sendChannelsToList
+      />
+
       <Channel EmojiPicker={EmojiPicker} emojiSearchIndex={SearchIndex}>
         <Window>
           <ChannelHeader />
           <MessageList />
-          <MessageInput />
+          <MessageInput audioRecordingEnabled />
         </Window>
+
         <Thread />
       </Channel>
     </Chat>
